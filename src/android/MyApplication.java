33882e772929f94e710477ed9cbacaee0aa5ded3
Lateph Herlino <lateph@gmail.com>
2020-06-24 11:28:36 +0700

    Update MyApplication.java

--- src/android/MyApplication.java
+++ src/android/MyApplication.java
@@ -2,11 +2,13 @@ package com.lateph;
 
 import android.app.Application;
 import android.util.Log;
+import com.facebook.soloader.SoLoader;
 
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         Log.d("MyApplication", "onCreate");
         super.onCreate();
+        SoLoader.init(this, false);
     }
 }
